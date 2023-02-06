f42cc57eac618c2d45caa551d0bc0a2824eb5e4f
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-06 10:31:26 -0500

    Initial Commit

---
+++ ListSwiftUI/ListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSwiftUIApp.swift
+//  ListSwiftUI
+//
+//  Created by Luis Angel Inga Mendoza on 6/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
